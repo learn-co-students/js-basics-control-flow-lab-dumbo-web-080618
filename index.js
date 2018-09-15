41680b6e39e8fc0fcf38f48679c9c17391a926cc
Madeline Celis <celiscb@vcu.edu>
2018-09-15 10:36:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet(numOfFeet){
+  if(numOfFeet <= 400){
+    return 'This one is on me!';
+  }else if(numOfFeet >= 2000 && numOfFeet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if (numOfFeet >= 2500){
+    return 'No can do.';
+  }
+};
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
+
+function switchOnCharmFromTip(tipStatement){
+  switch(tipStatement){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
